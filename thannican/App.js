d08237dc7ee295313a9a52be3635a8363406a0a0
kalees64 <vkalees64@gmail.com>
2024-06-28 12:04:31 +0530

    App launch

---
+++ thannican/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LandingScreen from "./screens/LandingScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>ThanniCan APP</Text>
+      <StatusBar style="auto" />
+      <LandingScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ thannican/screens/LandingScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LandingScreen = () => {
+  return (
+    <View>
+      <Text>Hello Kalees</Text>
+    </View>
+  );
+};
+
+export default LandingScreen;
